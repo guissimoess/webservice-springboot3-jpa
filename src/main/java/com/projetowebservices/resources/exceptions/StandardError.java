35d941a5f616a115e6c8ea91f82c35c66a99c3e1
Guilherme Simões <guilhermessimoescontato@gmail.com>
2023-06-14 11:45:21 -0300

    exception handling - delete

--- src/main/java/com/projetowebservices/resources/exceptions/StandardError.java
+++ src/main/java/com/projetowebservices/resources/exceptions/StandardError.java
@@ -11,7 +11,7 @@ public class StandardError implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
-    
+
     private Integer status;
     private String error;
     private String message;
